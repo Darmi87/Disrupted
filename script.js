2b6136bb6b37aa6840a2c528503ef11b7233136b
Darmi87 <107539562+Darmi87@users.noreply.github.com>
2025-02-06 00:14:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const influencers = document.querySelectorAll(".influencer");
+
+    influencers.forEach(influencer => {
+        influencer.addEventListener("click", function() {
+            this.classList.toggle("active");
+        });
+    });
+});
 
