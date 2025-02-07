97add78995e9344a11e1fd2ea05f5b5b4fcf0eec
Darmi87 <107539562+Darmi87@users.noreply.github.com>
2025-02-06 21:45:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.querySelector(".logo img");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > window.innerHeight * 0.5) {
+            logo.style.filter = "invert(0)"; /* Logo negro en secciones claras */
+        } else {
+            logo.style.filter = "invert(1)"; /* Logo blanco en el video */
+        }
+    });
+});
