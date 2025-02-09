e53daec9c3acc9b0dea422967caddc5e2f82aacb
Darmi87 <107539562+Darmi87@users.noreply.github.com>
2025-02-09 18:59:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const influencers = document.querySelectorAll(".influencer");
-
-    influencers.forEach(influencer => {
-        influencer.addEventListener("click", function() {
-            this.classList.toggle("active");
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const logo = document.querySelector(".logo img");
-
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > window.innerHeight * 0.5) {
-            logo.style.filter = "invert(0)"; /* Logo negro en secciones claras */
-        } else {
-            logo.style.filter = "invert(1)"; /* Logo blanco en el video */
-        }
-    });
-});
-document.addEventListener("DOMContentLoaded", function() {
     const preloader = document.querySelector(".preloader");
     const logoBox = document.querySelector(".logo-box");
     const content = document.querySelector(".content");
@@ -29,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
         preloader.classList.add("active");
     }, 1000);
 
-    // Comprimir logo y moverlo a la esquina
+    // Hacer aparecer el slogan después del efecto
     setTimeout(() => {
         preloader.classList.add("fade-out"); // Desvanece la pantalla de carga
         logoBox.classList.add("final-position"); // Mueve el logo a la esquina
         content.classList.add("visible"); // Muestra el contenido principal
-    }, 3000);
+    }, 3500);
 });
